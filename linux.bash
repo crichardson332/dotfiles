0781aed93e9eeed80d71eac77d598897b0355244
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2020-11-25 15:55:47 -0500

    fix pkg install loop

--- linux.bash
+++ linux.bash
@@ -30,9 +30,15 @@ PKGS=(
   xkbset
 )
 
+pkglist=''
+for pkg in "${PKGS[@]}"
+do
+   pkglist+=" $pkg"
+done
+
 sudo apt-get update
-sudo apt-get install -y $PKGS
+sudo apt-get install -y $pkglist
 
 # remap caps lock to ctrl
 # /usr/bin/setxkbmap -option caps:ctrl_modifier
-xset r rate 250 60
+xset r rate 250 40
