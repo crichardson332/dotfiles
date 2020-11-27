9a29e7550c98b18cc3c02e7c6ad885be36126bef
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2020-11-27 12:36:45 -0500

    fix symlinks

--- symlinks.bash
+++ symlinks.bash
@@ -7,6 +7,28 @@ ln -sfn "$DIR/.fonts" "$HOME/.fonts"
 ln -sfn "$DIR/.prompt.bash" "$HOME/.prompt.bash"
 ln -sfn "$DIR/.vimrc" "$HOME/.vimrc"
 
+# have to check if hard directories already exist, then remove them
+if [ -d "$HOME/.config/git" ]
+then
+  trash "$HOME/.config/git"
+fi
+if [ -d "$HOME/.config/i3" ]
+then
+  trash "$HOME/.config/i3"
+fi
+if [ -d "$HOME/.config/nautilus" ]
+then
+  trash "$HOME/.config/nautilus"
+fi
+if [ -d "$HOME/.config/rofi" ]
+then
+  trash "$HOME/.config/rofi"
+fi
+if [ -d "$HOME/.config/terminator" ]
+then
+  trash "$HOME/.config/terminator"
+fi
+
 ln -sfn "$DIR/.config/git" "$HOME/.config/git"
 ln -sfn "$DIR/.config/i3" "$HOME/.config/i3"
 ln -sfn "$DIR/.config/nautilus" "$HOME/.config/nautilus"
