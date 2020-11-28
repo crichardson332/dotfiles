804b4e6d577b44ba5f8be02ff0cfd1fd5c241839
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2020-11-28 12:13:42 -0500

    use loops for symlinks

--- symlinks.bash
+++ symlinks.bash
@@ -1,36 +1,34 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -sfn "$DIR/.bashrc" "$HOME/.bashrc"
-ln -sfn "$DIR/.bashrc_ssh" "$HOME/.bashrc_ssh"
-ln -sfn "$DIR/.dircolors" "$HOME/.dircolors"
-ln -sfn "$DIR/.fonts" "$HOME/.fonts"
-ln -sfn "$DIR/.prompt.bash" "$HOME/.prompt.bash"
-ln -sfn "$DIR/.vimrc" "$HOME/.vimrc"
+LINKS=(
+  .bashrc
+  .bashrc_ssh
+  .prompt.bash
+  .vimrc
+)
 
-# have to check if hard directories already exist, then remove them
-if [ -d "$HOME/.config/git" ]
-then
-  trash "$HOME/.config/git"
-fi
-if [ -d "$HOME/.config/i3" ]
-then
-  trash "$HOME/.config/i3"
-fi
-if [ -d "$HOME/.config/nautilus" ]
-then
-  trash "$HOME/.config/nautilus"
-fi
-if [ -d "$HOME/.config/rofi" ]
-then
-  trash "$HOME/.config/rofi"
-fi
-if [ -d "$HOME/.config/terminator" ]
-then
-  trash "$HOME/.config/terminator"
-fi
+DIRS=(
+  .dircolors
+  .fonts
+  .config/git
+  .config/i3
+  .config/nautilus
+  .config/rofi
+  .config/terminator
+)
 
-ln -sfn "$DIR/.config/git" "$HOME/.config/git"
-ln -sfn "$DIR/.config/i3" "$HOME/.config/i3"
-ln -sfn "$DIR/.config/nautilus" "$HOME/.config/nautilus"
-ln -sfn "$DIR/.config/rofi" "$HOME/.config/rofi"
-ln -sfn "$DIR/.config/terminator" "$HOME/.config/terminator"
+for dir in "${DIRS[@]}"
+do
+  # remove physical directories since they cant be overwritten with links
+  if [ -d "$HOME/$dir" ]
+  then
+    trash "$HOME/$dir"
+  fi
+  # add to list of links to be created
+  LINKS+=" $dir"
+done
+
+for link in "${LINKS[@]}"
+do
+  ln -sfn "$SCRIPTDIR/$link" "$HOME/$link"
+done
